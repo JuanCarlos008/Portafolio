93db4e5a31740f17aa4b87fb901efa393bdd9065
Juan Carlos HS <juancarloshs008@gmail.com>
2022-07-13 17:56:15 -0400

    fixed body overflow-x: hidden

--- src/components/ServiceCard/Card.jsx
+++ src/components/ServiceCard/Card.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { FaLaptopCode } from "react-icons/fa";
 import "./styles.scss";
 
-export const ServiceCard = () => {
+export const ServiceCard = ({ item }) => {
+	const { title, description, icon } = item;
 	return (
 		<article className='service-card'>
-			<div className='service-card__icon'>
-				<FaLaptopCode />
-			</div>
+			<div className='service-card__icon'>{icon}</div>
 			<div className='service-card__content'>
-				<h2 className='content-title'>Desarrolllo Web Frontend</h2>
-				<p className='content-info'>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, cum
-					neque animi tempore sequi officia esse inventore repellat facilis,
-					quia fugiat commodi veniam doloremque saepe.
-				</p>
+				<h2 className='content-title'>{title}</h2>
+				<p className='content-info'>{description}</p>
 			</div>
 		</article>
 	);
--- src/components/ServiceCard/Index.jsx
+++ src/components/ServiceCard/Index.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import { ServiceCard } from "./Card";
 import { MaxContainer } from "@components/containers/MaxContainer";
+import { CgDisplayFlex } from "react-icons/cg";
+import { FaLaptopCode } from "react-icons/fa";
+import { TbBrowserCheck } from "react-icons/tb";
+import { AiOutlineFormatPainter } from "react-icons/ai";
 import "./styles.scss";
+
+const data = [
+	{
+		id: 1,
+		title: "Web Development",
+		description:
+			"Websites are the most important part of your business. We can help you build a website that will help you grow your business.",
+		icon: <FaLaptopCode />,
+	},
+	{
+		id: 2,
+		title: "SEO",
+		description:
+			" We can help you build a website that will help you grow your business.",
+		icon: <TbBrowserCheck />,
+	},
+	{
+		id: 3,
+		title: "UI/UX",
+		description:
+			"We create user-friendly interfaces that are easy to use and intuitive.",
+		icon: <AiOutlineFormatPainter />,
+	},
+	{
+		id: 4,
+		title: "Responsive web design",
+		description:
+			"We create web applications that are user-friendly and easy to use.",
+		icon: <CgDisplayFlex />,
+	},
+];
 export const ServiceCards = () => {
 	return (
 		<div className='services section'>
@@ -9,10 +44,9 @@ export const ServiceCards = () => {
 				<div className='services__wrapper'>
 					<h2 className='section-title'>¿Qué ofrezco?</h2>
 					<div className='service-cards-wrapper'>
-						<ServiceCard />
-						<ServiceCard />
-						<ServiceCard />
-						<ServiceCard />
+						{data.map((item) => (
+							<ServiceCard key={item.id} item={item} />
+						))}
 					</div>
 				</div>
 			</MaxContainer>
