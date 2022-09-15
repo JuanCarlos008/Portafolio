e0c06286495ed65526bbb6b23963a003de578713
Juan Carlos HS <juancarloshs008@gmail.com>
2022-09-14 22:37:38 -0400

    fixed project image path

--- src/components/sections/Projects/Index.jsx
+++ src/components/sections/Projects/Index.jsx
@@ -13,12 +13,12 @@ export const ProjectSection = () => {
 						<h2 className='section-title'>Mí experiencia</h2>
 
 						<div className='project__cards-wrapper'>
-							<WorkCard img='/assets/images/projects/project_1.png' />
-							<WorkCard img='/assets/images/projects/project_2.png' />
-							<WorkCard img='/assets/images/projects/project_3.png' />
-							<WorkCard img='/assets/images/projects/project_4.png' />
-							<WorkCard img='/assets/images/projects/project_5.png' />
-							<WorkCard img='/assets/images/projects/project_6.png' />
+							<WorkCard img='./assets/images/projects/project_1.png' />
+							<WorkCard img='./assets/images/projects/project_2.png' />
+							<WorkCard img='./assets/images/projects/project_3.png' />
+							<WorkCard img='./assets/images/projects/project_4.png' />
+							<WorkCard img='./assets/images/projects/project_5.png' />
+							<WorkCard img='./assets/images/projects/project_6.png' />
 						</div>
 					</div>
 				</MaxContainer>
